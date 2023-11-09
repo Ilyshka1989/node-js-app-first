c08377943db0e803616d5d4ac5bf2ac21d827108
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2023-11-09 23:38:23 +0300

    add function greeting

--- app.js
+++ app.js
@@ -4,5 +4,21 @@ http.createServer(function(request,response)
 }).listen(3000, "127.0.0.1", function(){
     console.log("Должно работать");
 });
-  
 
+const greeting = require("./greeting");
+let currentDate = new Date();
+module.exports.currentDate = currentDate;
+module.exports.getMessage = function(name){
+    lethour = currentDate.getHours();
+    if(hour > 16)
+    return "Добрый вечер," + name;
+    else if(hour > 10)
+    return "Доброе день," + name;
+else 
+return "Доброе утро," + name;
+}
+const os = require("os");
+let userName = os.userInfo().username;
+
+console.log('Дата запроса: ${greeting.date}');
+console.log(greeting.getMessage(userName));
